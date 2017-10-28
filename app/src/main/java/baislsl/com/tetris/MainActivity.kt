8940335d9f52d1c64747a8712bd867175dc2c989
baislsl <baislsl666@gmail.com>
2017-10-28 14:40:19 +0800

    init

---
+++ app/src/main/java/baislsl/com/tetris/MainActivity.kt
@@ -0,0 +1,32 @@
+package baislsl.com.tetris
+
+import android.graphics.Color
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.text.InputType
+import android.view.Gravity
+import android.widget.Button
+import android.widget.EditText
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MainActivityUI().setContentView(this)
+    }
+}
+
+class MainActivityUI : AnkoComponent<MainActivity> {
+    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+        verticalLayout(theme = R.style.AppTheme) {
+            imageView(android.R.drawable.ic_menu_manage).lparams{
+                width = wrapContent
+                height = wrapContent
+                gravity = Gravity.CENTER
+            }
+        }.applyRecursively {  }
+
+    }
+}
